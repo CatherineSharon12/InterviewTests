d7a4b526ff532ba228e2459032d8311285c1a4ac
Catherine Sharon <Catherine Sharon@DELLE7240>
2019-02-26 19:20:22 -0500

    Simplified the code in GraduationTracker Added 2 more unit tests Updated the testMethod to fetch data from the repository when possible Changed the structure of a few objects Removed unused usings

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,64 +12,8 @@ namespace GraduationTracker.Tests.Unit
         public void TestHasCredits()
         {
             var tracker = new GraduationTracker();
-
-            var diploma = new Diploma
-            {
-                Id = 1,
-                Credits = 4,
-                Requirements = new int[] { 100, 102, 103, 104 }
-            };
-
-            var students = new[]
-            {
-               new Student
-               {
-                   Id = 1,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=95 },
-                        new Course{Id = 2, Name = "Science", Mark=95 },
-                        new Course{Id = 3, Name = "Literature", Mark=95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
-                   }
-               },
-               new Student
-               {
-                   Id = 2,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=80 },
-                        new Course{Id = 2, Name = "Science", Mark=80 },
-                        new Course{Id = 3, Name = "Literature", Mark=80 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
-                   }
-               },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
-                }
-            }
-
-
-            //tracker.HasGraduated()
-        };
+            var diploma = Repository.GetDiploma(1);
+            Student[] students = { Repository.GetStudent(1), Repository.GetStudent(2), Repository.GetStudent(3), Repository.GetStudent(4) };
             
             var graduated = new List<Tuple<bool, STANDING>>();
 
@@ -77,12 +21,33 @@ namespace GraduationTracker.Tests.Unit
             {
                 graduated.Add(tracker.HasGraduated(diploma, student));      
             }
-
-            
-            Assert.IsFalse(graduated.Any());
+            Assert.AreEqual(2, graduated.Where(g => !g.Item1 && g.Item2.Equals(STANDING.Remedial)).Count()); // only two students must have failed
+            Assert.IsTrue(graduated[2].Item2.Equals(STANDING.SumaCumLaude) && graduated[3].Item2.Equals(STANDING.Average));
 
         }
 
+        [TestMethod]
+        public void TestStudentHasOptedForAllRequiredCourses()
+        {
+            var tracker = new GraduationTracker();
+            var diploma = Repository.GetDiploma(2);
+            Student student = Repository.GetStudent(5);
 
+            Tuple<bool, STANDING> result = tracker.HasGraduated(diploma, student);
+            Assert.AreEqual(new Tuple<bool, STANDING>(false, STANDING.Remedial), result);
+
+        }
+
+        [TestMethod]
+        public void TestStudentHasOptedForAtLeastOneCourses()
+        {
+            var tracker = new GraduationTracker();
+            var diploma = Repository.GetDiploma(2);
+            Student student = Repository.GetStudent(5);
+
+            Tuple<bool, STANDING> result = tracker.HasGraduated(diploma, student);
+            Assert.AreEqual(new Tuple<bool, STANDING>(false, STANDING.Remedial), result);
+
+        }
     }
 }
--- GraduationTracker/GraduationTracker/GraduationTracker.cs
+++ GraduationTracker/GraduationTracker/GraduationTracker.cs
@@ -6,60 +6,59 @@ using System.Threading.Tasks;
 
 namespace GraduationTracker
 {
-    public partial class GraduationTracker
+    public class GraduationTracker
     {   
         public Tuple<bool, STANDING>  HasGraduated(Diploma diploma, Student student)
         {
+            if (student.Courses == null || student.Courses.Count() == 0)
+            {
+                return new Tuple<bool, STANDING>(false, STANDING.Remedial);
+            }
             var credits = 0;
             var average = 0;
-        
-            for(int i = 0; i < diploma.Requirements.Length; i++)
-            {
-                for(int j = 0; j < student.Courses.Length; j++)
-                {
-                    var requirement = Repository.GetRequirement(diploma.Requirements[i]);
+            int TotalNumberOfCourses = 0;
 
-                    for (int k = 0; k < requirement.Courses.Length; k++)
-                    {
-                        if (requirement.Courses[k] == student.Courses[j].Id)
-                        {
-                            average += student.Courses[j].Mark;
-                            if (student.Courses[j].Mark > requirement.MinimumMark)
-                            {
-                                credits += requirement.Credits;
-                            }
-                        }
-                    }
+            foreach (Requirement r in diploma.Requirements)
+            {
+                //if the student does not have all the courses in the requirement then they do not graduate
+                Course[] coursesSelected = student.Courses.Where(c => r.Courses.Contains(c.Id)).ToArray<Course>();
+                if (coursesSelected.Count() == 0)
+                {
+                    return new Tuple<bool, STANDING>(false, STANDING.Remedial);
                 }
+
+                if (coursesSelected.Count() != r.Courses.Count())
+                {
+                    return new Tuple<bool, STANDING>(false, STANDING.Remedial);
+                }
+
+                average += coursesSelected.Select(c => c.Mark).Sum();
+                int passedCourses = coursesSelected.Where(c => c.Mark > r.MinimumMark).Count();
+                credits += passedCourses * r.Credits; //i've assumed that you only get credit points for the requirement if you complete all the courses in the requirement
+                TotalNumberOfCourses += coursesSelected.Count();
             }
 
-            average = average / student.Courses.Length;
+            average = average / TotalNumberOfCourses;
 
-            var standing = STANDING.None;
-
-            if (average < 50)
-                standing = STANDING.Remedial;
-            else if (average < 80)
-                standing = STANDING.Average;
-            else if (average < 95)
-                standing = STANDING.MagnaCumLaude;
-            else
-                standing = STANDING.MagnaCumLaude;
-
-            switch (standing)
+            //if the student does not have the number of credits necessary for the diploma then they do not graduate
+            if (credits < diploma.CreditsToGraduate)
             {
-                case STANDING.Remedial:
-                    return new Tuple<bool, STANDING>(false, standing);
-                case STANDING.Average:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.SumaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.MagnaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
+                return new Tuple<bool, STANDING>(false, STANDING.Remedial);
+            }
 
+            switch (average)
+            {
+                case int n when (n < 50):
+                    return new Tuple<bool, STANDING>(false, STANDING.Remedial);
+                case int n when (n < 80):
+                    return new Tuple<bool, STANDING>(true, STANDING.Average);
+                case int n when (n < 90):
+                    return new Tuple<bool, STANDING>(true, STANDING.MagnaCumLaude);
+                case int n when (n <= 100):
+                    return new Tuple<bool, STANDING>(true, STANDING.SumaCumLaude);
                 default:
-                    return new Tuple<bool, STANDING>(false, standing);
-            } 
+                    return new Tuple<bool, STANDING>(true, STANDING.Remedial);
+            }
         }
     }
 }
